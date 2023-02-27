6fbd73985bae1685c786b9640a5e32fc22f9ed37
symphony2233 <47616850+symphony2233@users.noreply.github.com>
2023-02-27 17:34:40 +0800

    hello项目初始化

--- JAVA/learn/base_hello/src/main/java/com/example/hello/controller/HelloController.java
+++ JAVA/learn/base_hello/src/main/java/com/example/hello/controller/HelloController.java
@@ -1,8 +1,6 @@
-package com.example.hello_sprintboot.controller;
+package com.example.hello.controller;
 
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
