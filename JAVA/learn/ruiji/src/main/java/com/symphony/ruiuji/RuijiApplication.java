b619ccf467b40add1d54ea60a87f22947d7f6e17
symphony2233 <47616850+symphony2233@users.noreply.github.com>
2023-03-02 16:45:20 +0800

    测试基础环境，HelloControler

---
+++ JAVA/learn/ruiji/src/main/java/com/symphony/ruiuji/RuijiApplication.java
@@ -0,0 +1,13 @@
+package com.symphony.ruiuji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuijiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuijiApplication.class, args);
+    }
+
+}
